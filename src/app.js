517526d8abf9ac92ed2900ff34b0d8cb64d7c9fd
Marina Master <marina777master@mail.ru>
2023-09-27 20:45:57 -0400

    complited excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#text").innerHTML = `<p>${excuses()}</p>`;
 };
+
+function excuses() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let generateWho = Math.floor(Math.random() * who.length);
+  let generateAction = Math.floor(Math.random() * action.length);
+  let generateWhat = Math.floor(Math.random() * what.length);
+  let generateWhen = Math.floor(Math.random() * when.length);
+
+  return `${who[generateWho]} ${action[generateAction]} ${what[generateWhat]} ${when[generateWhen]}`;
+}
+console.log(excuses());
